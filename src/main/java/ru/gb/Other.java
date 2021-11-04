a9b4a3f79311b8b07b77e6ac2bb45dc46316aa0f
AlexApukhtin <aac94@yandex.ru>
2021-11-04 23:01:59 +0300

    animal Count++

--- src/main/java/ru/gb/Other.java
+++ src/main/java/ru/gb/Other.java
@@ -1,4 +1,35 @@
 package ru.gb;
 
-public class Other {
+public class Other extends Animal {
+
+    private static int otherScore;
+
+    public Other(String name, int run, int swim, String view) {
+        super(name, run, swim);
+        setView(view);
+        otherScore++;
+    }
+
+    public static int getOtherScore() {
+        return otherScore;
+    }
+
+    public void setView(String view) {
+        if (view.length() != 0) this.view = view;
+    }
+
+    @Override
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    @Override
+    public void setSwim(int swim) {
+        this.swim = swim;
+    }
+
+    @Override
+    public void print(int score, int length) {
+        System.out.printf("Это %s. %s пробежал %dм. и проплыл %dм.\n", getView(), getName(), getRun(), getSwim());
+    }
 }
